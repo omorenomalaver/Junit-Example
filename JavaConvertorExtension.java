4af0d4eaf16fb4e2dde8036fd2cd9b64cd9cd719
omorenomalaver <omorenomalaver@gmail.com>
2015-06-26 11:14:53 +0800

    Create JavaConvertorExtension.java

---
+++ JavaConvertorExtension.java
@@ -0,0 +1,15 @@
+import java.util.Iterator;
+import javax.swing.JOptionPane;
+
+public class JavaConvertorExtension {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Convertor myConvertor =  new Convertor();
+        myConvertor.menuOption();
+    }
+    
+}
